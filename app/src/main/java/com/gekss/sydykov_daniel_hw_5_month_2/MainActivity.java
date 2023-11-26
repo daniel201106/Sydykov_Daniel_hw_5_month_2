e2d14a26baaf69f51ab68b94d6ca5a439c123d2d
daniel201106 <danielsydykov757@gmail.com>
2023-11-26 18:41:39 +0600

    Initial commit

---
+++ app/src/main/java/com/gekss/sydykov_daniel_hw_5_month_2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gekss.sydykov_daniel_hw_5_month_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
